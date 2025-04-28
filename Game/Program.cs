17a333bcdd8fa96ce80a971d14c594bc7c9cc762
Purpkin <guiejo2020@gmail.com>
2025-04-27 22:42:26 -0300

    Basic FNA starter + include prebuilt windows dll's on build

--- Game/Program.cs
+++ Game/Program.cs
@@ -1,3 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.Xna.Framework;
 
 Console.WriteLine("Hello, World!");
+
+using (Game game = new Game())
+{
+    var g = new GraphicsDeviceManager(game);
+    game.Run();
+}
